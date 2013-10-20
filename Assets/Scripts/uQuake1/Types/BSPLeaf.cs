dcec9047c4ae23b0aa8c8b9971c9b76123c51e9e
mikezila <donoteatmikezila@gmail.com>
2013-10-20 15:43:40 -0400

    vis work
    still not working, but more work

--- Assets/Scripts/uQuake1/Types/BSPLeaf.cs
+++ Assets/Scripts/uQuake1/Types/BSPLeaf.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
+
 public class BSPLeaf
 {
     public int type;
@@ -13,18 +14,15 @@ public class BSPLeaf
     public int lface_index;
     public int num_lfaces;
 
-    public BSPLeaf(int type, int vislist, byte[] minmaxs, ushort lface_index, ushort num_lfaces)
+    public BSPLeaf(int type, int vislist, Vector3 mins, Vector3 maxs, ushort lface_index, ushort num_lfaces)
     {
         this.type = type;
         this.vislist = vislist;
         this.lface_index = (int)lface_index;
         this.num_lfaces = (int)num_lfaces;
 
-        mins = new Vector3((float)minmaxs[0], (float)minmaxs[1], (float)minmaxs[2]);
-        maxs = new Vector3((float)minmaxs[3], (float)minmaxs[4], (float)minmaxs[5]);
-
-        mins = SwizVert(mins);
-        maxs = SwizVert(maxs);
+        this.mins = SwizVert(mins);
+        this.maxs = SwizVert(maxs);
     }
 
     private Vector3 SwizVert(Vector3 vert)
@@ -38,6 +36,6 @@ public class BSPLeaf
 
     public override string ToString()
     {
-        return "Type: " + type.ToString() + " Vislist: " + vislist.ToString() + " Face Index/Count: " + lface_index.ToString() + "/" + num_lfaces.ToString();
+        return "Type: " + type.ToString() + " Vislist: " + vislist.ToString() + " Mins/Maxs: " + mins.ToString() + " / " + maxs.ToString();
     }
 }
